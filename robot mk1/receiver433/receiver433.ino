e9ced2fdc3b0930ba4babd758d6c768e894e8449
dkurchigin <kurchigin.dmitry@yandex.ru>
2018-12-24 21:23:27 +0300

    add moveRobot func

--- robot mk1/receiver433/receiver433.ino
+++ robot mk1/receiver433/receiver433.ino
@@ -1,12 +1,17 @@
 #include <VirtualWire.h>
+#include <ServoTimer2.h>
 
 #define LED_PIN 13
 #define TOP_RIGHT_MSFT A0
 #define TOP_LEFT_MSFT A1
 #define BOTTOM_RIGHT_MSFT A2
 #define BOTTOM_LEFT_MSFT A3
+#define RIGHT_SERVO 9
+#define LEFT_SERVO 10
 
 int timer;
+ServoTimer2 l_servo;
+ServoTimer2 r_servo;
 
 void setup() {
   Serial.begin(9600);
@@ -19,13 +24,21 @@ void setup() {
   pinMode(TOP_LEFT_MSFT, OUTPUT);
   pinMode(BOTTOM_RIGHT_MSFT, OUTPUT);
   pinMode(BOTTOM_LEFT_MSFT, OUTPUT);
-  analogWrite(TOP_RIGHT_MSFT, 255);
-  analogWrite(TOP_LEFT_MSFT, 255);
+  analogWrite(TOP_RIGHT_MSFT, 0);
+  analogWrite(TOP_LEFT_MSFT, 0);
   analogWrite(BOTTOM_RIGHT_MSFT, 0);
   analogWrite(BOTTOM_LEFT_MSFT, 0);
+  moveRobot("all_back");
 }
 
 void loop() {
+  moveRobot("forward");
+  moveRobot("back");
+  moveRobot("left");
+  moveRobot("stop");
+  moveRobot("right");
+  moveRobot("stop");
+  
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
   if (vw_get_message(buf, &buflen)) {  
@@ -33,44 +46,96 @@ void loop() {
       if (buf[2] == '4') {
         if (buf[3] == '0') {
           digitalWrite(LED_PIN,HIGH);
-          moveMotor("back");
+          moveRobot("back");
           delay(100);  
         } else if (buf[3] == '1') {
           digitalWrite(LED_PIN,HIGH);
-          moveMotor("forward");
+          moveRobot("forward");
           delay(100);  
         } else if (buf[3] == '2') {
           digitalWrite(LED_PIN,HIGH);
-          moveMotor("forward");
+          moveRobot("right");
           delay(100);
         } else if (buf[3] == '3') {
           digitalWrite(LED_PIN,HIGH);
-          moveMotor("forward");
+          moveRobot("left");
           delay(100);
         }     
       }
     }
   } else {
-    moveMotor("stop");
+    moveRobot("stop");
   }
 }
 
-void moveMotor(String command) {
+void moveRobot(String command) {
   if (command == "forward") {
+    l_servo.attach(LEFT_SERVO);
+    r_servo.attach(RIGHT_SERVO);
+    l_servo.write(900);/*have contact*/
+    r_servo.write(1600);/*have contact*/
+    delay(300);
+    l_servo.detach();
+    r_servo.detach();
     analogWrite(TOP_RIGHT_MSFT, 255);
     analogWrite(TOP_LEFT_MSFT, 0);
-    analogWrite(BOTTOM_RIGHT_MSFT, 255);
-    analogWrite(BOTTOM_LEFT_MSFT, 0);
-  }
-  if (command == "back") {
-    analogWrite(TOP_RIGHT_MSFT, 0);
-    analogWrite(TOP_LEFT_MSFT, 255);
     analogWrite(BOTTOM_RIGHT_MSFT, 0);
     analogWrite(BOTTOM_LEFT_MSFT, 255);
-  }if (command == "stop") {
-    analogWrite(TOP_RIGHT_MSFT, 255);
+    delay(300);
+  }
+  if (command == "back") {
+    l_servo.attach(LEFT_SERVO);
+    r_servo.attach(RIGHT_SERVO);
+    l_servo.write(900);/*have contact*/
+    r_servo.write(1600);/*have contact*/
+    delay(300);
+    l_servo.detach();
+    r_servo.detach();
+    analogWrite(TOP_RIGHT_MSFT, 0);
     analogWrite(TOP_LEFT_MSFT, 255);
+    analogWrite(BOTTOM_RIGHT_MSFT, 255);
+    analogWrite(BOTTOM_LEFT_MSFT, 0);
+    delay(300);
+  }
+  if (command == "stop") {
+    analogWrite(TOP_RIGHT_MSFT, 0);
+    analogWrite(TOP_LEFT_MSFT, 0);
     analogWrite(BOTTOM_RIGHT_MSFT, 0);
     analogWrite(BOTTOM_LEFT_MSFT, 0);
+    l_servo.attach(LEFT_SERVO);
+    r_servo.attach(RIGHT_SERVO);
+    l_servo.write(1500);/*dont have contact*/
+    r_servo.write(1000);/*dont have contact*/
+    delay(300);
+    l_servo.detach();
+    r_servo.detach();
+  }
+  if (command == "left") {
+    l_servo.attach(LEFT_SERVO);
+    r_servo.attach(RIGHT_SERVO);
+    l_servo.write(900);/*have contact*/
+    r_servo.write(1000);/*dont have contact*/
+    delay(300);
+    l_servo.detach();
+    r_servo.detach();
+    analogWrite(TOP_RIGHT_MSFT, 255);
+    analogWrite(TOP_LEFT_MSFT, 0);
+    analogWrite(BOTTOM_RIGHT_MSFT, 0);
+    analogWrite(BOTTOM_LEFT_MSFT, 255);
+    delay(300);
+  }
+  if (command == "right") {
+    l_servo.attach(LEFT_SERVO);
+    r_servo.attach(RIGHT_SERVO);
+    l_servo.write(1500);/*dont have contact*/
+    r_servo.write(1600);/*have contact*/
+    delay(300);
+    l_servo.detach();
+    r_servo.detach();
+    analogWrite(TOP_RIGHT_MSFT, 255);
+    analogWrite(TOP_LEFT_MSFT, 0);
+    analogWrite(BOTTOM_RIGHT_MSFT, 0);
+    analogWrite(BOTTOM_LEFT_MSFT, 255);
+    delay(300);
   }
 }
